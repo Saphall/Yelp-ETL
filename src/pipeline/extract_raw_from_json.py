4dd5f7ec655c5d0cb50acc22f2498d67f688ae97
Saphall <safalsakha@gmail.com>
2021-10-08 07:59:16 +0545

    Updated extract_business

--- src/pipeline/extract_raw_from_json.py
+++ src/pipeline/extract_raw_from_json.py
@@ -5,6 +5,9 @@ from utils.database_connection import *
 from utils.file_content_toString import *
 from psycopg2.extras import execute_batch
 from alive_progress import alive_bar
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 BATCH_SIZE = 100000
@@ -12,10 +15,10 @@ BATCH_SIZE = 100000
 
 def extract_raw_from_json(table_name, file_path, query_path):
     """
-        This function extracts json file into raw_tables, 
-        if given to which table to extract (table_name), 
-        json file path (file_path), 
-        sql query to insert into table (query_path).
+    This function extracts json file into raw_tables, 
+    :param table_name: The name of table to extract json. 
+    :param file_path: The json file path. 
+    :param query_path: The sql query path to help insert into the table.
     """
     try:
 
@@ -30,37 +33,44 @@ def extract_raw_from_json(table_name, file_path, query_path):
             f"\n[{SUCCESS}INFO{END}] {BOLD}Extract ({file_path}: {int(completion)} rows) into raw.{END}")
 
         # Empty table before extraction
-        cur.execute(f'TRUNCATE TABLE {table_name} RESTART IDENTITY;')
+        cur.execute(f'TRUNCATE TABLE {table_name};')
 
         # Get content of query_path as string
         extract_query = file_content_toString(query_path)
 
-        i = 0
-        filtered_data = []
+        # Extract with temp_table to handle nested json
+        if table_name == 'raw_business':
+            subprocess.Popen(
+                f'psql {os.getenv("DATABASE_USER") } -h {os.getenv("HOST")} -d {os.getenv("DATABASE_NAME")} -f {query_path}', 
+                  shell=True)
 
-        # Read json file and batch insert data.
-        with open(file_path, 'r') as json_file:
-            with alive_bar(int(completion)) as bar:
-                for line in (json_file):
-                    data = []
-                    data.append(json.loads(line))
-                    data = [[str(ele) for ele in item.values()]
-                            for item in data]
-                    filtered_data.append(data[0])
-                    bar()
+        else:
+            i = 0
+            filtered_data = []
 
-                    if i == BATCH_SIZE:
-                        execute_batch(cur, extract_query, filtered_data)
-                        con.commit()
-                        filtered_data = []
-                        i = 0
-                        continue
-                    i += 1
+            # Read json file and batch insert data.
+            with open(file_path, 'r', encoding='utf8') as json_file:
+                with alive_bar(int(completion)) as bar:
+                    for line in (json_file):
+                        data = []
+                        data.append(json.loads(line))
+                        data = [[str(value) for value in item.values()]
+                                for item in data]
+                        filtered_data.append(data[0])
+                        bar()
 
-        # Extract remaining data if less than batch size.
-        if len(filtered_data) != 0:
-            execute_batch(cur, extract_query, filtered_data)
-            con.commit()
+                        if i == BATCH_SIZE:
+                            execute_batch(cur, extract_query, filtered_data)
+                            con.commit()
+                            filtered_data = []
+                            i = 0
+                            continue
+                        i += 1
+
+            # Extract remaining data if less than batch size.
+            if len(filtered_data) != 0:
+                execute_batch(cur, extract_query, filtered_data)
+                con.commit()
 
         print(f'[{SUCCESS}INFO{END}] {BOLD}Extraction Completed !{END}\n')
         databaseDisconnect(con, cur)
@@ -70,10 +80,10 @@ def extract_raw_from_json(table_name, file_path, query_path):
 
 
 if __name__ == '__main__':
-    extract_raw_from_json('raw_business', '../../data/business.json',
-                          '../sql/queries/extract_raw_business.sql')
     extract_raw_from_json('raw_checkin', '../../data/checkin.json',
                           '../sql/queries/extract_raw_checkin.sql')
+    extract_raw_from_json('raw_business', '../../data/business.json',
+                          '../sql/queries/extract_raw_business.sql')
     extract_raw_from_json('raw_photos', '../../data/photos.json',
                           '../sql/queries/extract_raw_photos.sql')
     extract_raw_from_json('raw_tip', '../../data/tip.json',
--- src/sql/queries/extract_raw_business.sql
+++ src/sql/queries/extract_raw_business.sql
@@ -1,2 +1,21 @@
+TRUNCATE TABLE temp_business;
+\COPY temp_business FROM   '/home/saphal/1Saphal/20Data_Engineering/JSON/business.json';
+TRUNCATE TABLE raw_business;
 INSERT INTO raw_business
-VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+  SELECT   
+  data ->> 'business_id',
+  data ->> 'name',
+  data ->> 'address',
+  data ->> 'city',
+  data ->> 'state',
+  data ->> 'postal_code',
+  data ->> 'latitude',
+  data ->> 'longitude',
+  data ->> 'stars',
+  data ->> 'review_count',
+  data ->> 'is_open',
+  data -> 'attributes',
+  data ->> 'categories',
+  data -> 'hours'
+FROM temp_business ;
+
